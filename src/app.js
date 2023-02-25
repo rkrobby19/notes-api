245ca7484b3b2b7ba94a1d58afe9bd20f18906a6
rkrobby19 <rizkyrobby19@gmail.com>
2023-02-25 10:24:28 +0700

    Add access control method for put and delete

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
   next();
 });
